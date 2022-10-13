aaa353f446d5ae20fba4c76723a3200a4656acdd
Atsushi KAWASAKI <ak@xlix.org>
2022-10-14 00:20:43 +0900

    cause panic.

--- wasmple/src/lib.rs
+++ wasmple/src/lib.rs
@@ -1,7 +1,9 @@
 mod console;
 
 #[no_mangle]
+#[allow(unreachable_code)]
 pub extern "C" fn hello() {
+    panic!("OMG - あたふた・・・");
     console::log(format!("LOG - ログだよ！"));
     console::debug(format!("DEBUG - デバッグだよ！"));
     console::info(format!("INFO - インフォメーションだよ！"));
