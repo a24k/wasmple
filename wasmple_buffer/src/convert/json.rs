04c42e0ba7cbda9e5380622953ee891f572e5c3c
Atsushi KAWASAKI <ak@xlix.org>
2022-11-15 22:43:25 +0900

    add tests for buffer::convert::json.

--- wasmple_buffer/src/convert/json.rs
+++ wasmple_buffer/src/convert/json.rs
@@ -16,3 +16,65 @@ where
         super::from(serde_json::to_string(self).ok()?)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rstest::*;
+
+    use super::super::{from, into, BufferManager, JsonConvertee};
+    use serde::{Deserialize, Serialize};
+    use serde_json::{json, Value};
+
+    #[rstest]
+    #[case(json!({}))]
+    #[case(json!({"a": "b", "c": "d"}))]
+    #[case(json!({"x": 0, "y": 1}))]
+    fn convert_json_untyped(#[case] input: Value) {
+        let ptr = from(input.clone());
+
+        match ptr {
+            None => panic!("ptr: Option<BufferPtr> will be Some."),
+            Some(ptr) => {
+                let output = into::<Value>(ptr);
+
+                match output {
+                    None => panic!("ptr: Option<String> will be Some."),
+                    Some(output) => {
+                        assert_eq!(input, output);
+                    }
+                }
+
+                BufferManager::lock().dealloc(ptr);
+            }
+        }
+    }
+
+    #[derive(Serialize, Deserialize, JsonConvertee, Debug, PartialEq, Eq, Clone)]
+    struct TypedJson {
+        name: String,
+        age: usize,
+    }
+
+    #[rstest]
+    #[case(TypedJson{name: String::from("James"), age: 25})]
+    #[case(TypedJson{name: String::from("太郎"), age: 35})]
+    fn convert_json_typed(#[case] input: TypedJson) {
+        let ptr = from(input.clone());
+
+        match ptr {
+            None => panic!("ptr: Option<BufferPtr> will be Some."),
+            Some(ptr) => {
+                let output = into::<TypedJson>(ptr);
+
+                match output {
+                    None => panic!("ptr: Option<String> will be Some."),
+                    Some(output) => {
+                        assert_eq!(input, output);
+                    }
+                }
+
+                BufferManager::lock().dealloc(ptr);
+            }
+        }
+    }
+}
