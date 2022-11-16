d355d00e59e366eb7166926d970d6f89cdcc6427
Atsushi KAWASAKI <ak@xlix.org>
2022-11-16 10:31:20 +0900

    refactor String convert methods for better testing.

--- wasmple/src/lib.rs
+++ wasmple/src/lib.rs
@@ -28,9 +28,8 @@ pub extern "C" fn convert(input_ptr: BufferPtr) -> BufferPtr {
 fn _convert(input_ptr: BufferPtr) -> Option<BufferPtr> {
     let input: FnConvertParameters = wasmple_buffer::into(input_ptr)?;
 
-    let interleaved: String = input.a.chars().interleave(input.b.chars()).collect();
-
-    let reversed: String = interleaved.chars().rev().collect();
+    let interleaved = _interleave(input.a, input.b);
+    let reversed = _reverse(interleaved.clone());
 
     let output = FnConvertReturns {
         interleaved,
@@ -38,6 +37,14 @@ fn _convert(input_ptr: BufferPtr) -> Option<BufferPtr> {
     };
 
     wasmple_buffer::from(output)
+}
+
+fn _interleave(input_a: String, input_b: String) -> String {
+    input_a.chars().interleave(input_b.chars()).collect()
+}
+
+fn _reverse(input: String) -> String {
+    input.chars().rev().collect()
 }
 
 #[cfg(test)]
@@ -48,15 +55,28 @@ mod tests {
     use wasmple_buffer::{from, into, BufferManager};
 
     #[rstest]
-    #[case(
-        FnConvertReturns {
-            interleaved: String::default(),
-            reversed: String::default()
-        },
-        FnConvertParameters {
-            a: String::default(),
-            b: String::default()
-        })]
+    #[case("", "", "")]
+    #[case("abc", "abc", "")]
+    #[case("123", "", "123")]
+    #[case("12345678", "1357", "2468")]
+    #[case("a1b2c345", "abc", "12345")]
+    #[case("a1b2c3de", "abcde", "123")]
+    #[case("パタトクカシーー🚔🚖", "パトカー🚔", "タクシー🚖")]
+    fn interleave(#[case] estimated: String, #[case] input_a: String, #[case] input_b: String) {
+        assert_eq!(estimated, super::_interleave(input_a, input_b));
+    }
+
+    #[rstest]
+    #[case("", "")]
+    #[case("87654321", "12345678")]
+    #[case("おえういあ", "あいうえお")]
+    #[case("土金木水火月日", "日月火水木金土")]
+    #[case("😋😊😁", "😁😊😋")]
+    fn reverse(#[case] estimated: String, #[case] input: String) {
+        assert_eq!(estimated, super::_reverse(input));
+    }
+
+    #[rstest]
     #[case(
         FnConvertReturns {
             interleaved: String::from("12345678"),
