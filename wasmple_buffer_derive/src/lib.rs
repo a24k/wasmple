e2beac41ae9a0bb2fb0e047e4118481fa5169f1d
Atsushi KAWASAKI <ak@xlix.org>
2022-11-17 15:19:02 +0900

    modify derive macro's parameter name.

--- wasmple_buffer_derive/src/lib.rs
+++ wasmple_buffer_derive/src/lib.rs
@@ -3,10 +3,10 @@ use quote::quote;
 use syn::DeriveInput;
 
 #[proc_macro_derive(JsonConvertee)]
-pub fn derive_json_convertee(input: TokenStream) -> TokenStream {
-    let input: DeriveInput = syn::parse(input).unwrap();
+pub fn derive_json_convertee(item: TokenStream) -> TokenStream {
+    let item: DeriveInput = syn::parse(item).unwrap();
 
-    let ident = input.ident;
+    let ident = item.ident;
 
     let impl_json_convertee = quote! {
         impl JsonConvertee for #ident {}
