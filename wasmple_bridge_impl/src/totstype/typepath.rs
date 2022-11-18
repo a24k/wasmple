9e99611fb77df9f7a35a594f3a36c2710c8c6c55
Atsushi KAWASAKI <ak@xlix.org>
2022-11-18 21:32:46 +0900

    generally Item has only one pathsegment.

--- wasmple_bridge_impl/src/totstype/typepath.rs
+++ wasmple_bridge_impl/src/totstype/typepath.rs
@@ -7,33 +7,31 @@ use crate::unsupported;
 
 impl ToTsType for TypePath {
     fn to_tstype_token_stream(&self) -> TokenStream {
-        let segs = &self.path.segments;
-
-        if segs.len() != 1 {
-            unsupported!(self);
-        }
-
-        segs.last()
-            .map_or(quote! {}, |seg| match seg.ident.to_string().as_str() {
-                "usize" => quote! { number },
-                "i8" => quote! { number },
-                "u8" => quote! { number },
-                "i16" => quote! { number },
-                "u16" => quote! { number },
-                "i32" => quote! { number },
-                "u32" => quote! { number },
-                "i64" => quote! { number },
-                "u64" => quote! { number },
-                "f32" => quote! { number },
-                "f64" => quote! { number },
-                "bool" => quote! { boolean },
-                "String" => quote! { string },
-                "Vec" => unsupported!(self), // to be recognized as array[]
-                _ => {
-                    let ident = &seg.ident;
-                    quote! { #ident }
+        match self.path.segments.last() {
+            None => unsupported!(self),
+            Some(seg) => {
+                match seg.ident.to_string().as_str() {
+                    "usize" => quote! { number },
+                    "i8" => quote! { number },
+                    "u8" => quote! { number },
+                    "i16" => quote! { number },
+                    "u16" => quote! { number },
+                    "i32" => quote! { number },
+                    "u32" => quote! { number },
+                    "i64" => quote! { number },
+                    "u64" => quote! { number },
+                    "f32" => quote! { number },
+                    "f64" => quote! { number },
+                    "bool" => quote! { boolean },
+                    "String" => quote! { string },
+                    "Vec" => unsupported!(self), // to be recognized as array[]
+                    _ => {
+                        let ident = &seg.ident;
+                        quote! { #ident }
+                    }
                 }
-            })
+            }
+        }
     }
 }
 
@@ -45,10 +43,6 @@ mod tests {
     use quote::quote;
 
     #[rstest]
-    #[should_panic(expected = "unsupported TypePath")]
-    #[case(quote! {}, quote! { unknown::unknown })]
-    #[should_panic(expected = "unsupported TypePath")]
-    #[case(quote! {}, quote! { usize::usize })]
     #[case(quote! { number }, quote! { usize })]
     #[case(quote! { number }, quote! { i8 })]
     #[case(quote! { number }, quote! { u8 })]
@@ -64,6 +58,8 @@ mod tests {
     #[case(quote! { string }, quote! { String })]
     #[case(quote! { unknown }, quote! { unknown })]
     #[case(quote! { TestStruct }, quote! { TestStruct })]
+    #[case(quote! { unknown }, quote! { unknown::unknown })]
+    #[case(quote! { number }, quote! { usize::usize })]
     fn convert_to_tstype(#[case] expected: TokenStream, #[case] item: TokenStream) {
         let item: TypePath = syn::parse2(item).unwrap();
         assert_eq!(
