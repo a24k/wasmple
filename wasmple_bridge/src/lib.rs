a0212707c97485dc26a36c3fe2fbbc498b09f036
Atsushi KAWASAKI <ak@xlix.org>
2022-12-24 07:45:21 +0900

    rename inventory struct to TsScript.

--- wasmple_bridge/src/lib.rs
+++ wasmple_bridge/src/lib.rs
@@ -2,16 +2,16 @@ pub use wasmple_bridge_attribute::wasmple_bridge;
 
 pub use inventory;
 
-pub struct TsString {
+pub struct TsScript {
     #[allow(dead_code)] // temporary avoid warning
     script: &'static str,
 }
 
-impl TsString {
+impl TsScript {
     pub const fn new(script: &'static str) -> Self {
-        TsString { script }
+        TsScript { script }
     }
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-inventory::collect!(TsString);
+inventory::collect!(TsScript);
